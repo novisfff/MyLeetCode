b12eda58f81c55813bf469220a717354a8853bd3
npuZhangYuFei <156125813@qq.com>
2020-09-16 23:20:48 +0800

    solution 1

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,18 @@ import utils.*;
 public class Main {
 
     public static void main(String[] args) {
+
         //此处输入不计算时间的初始化代码
+
+        Solution solution = new Solution();
+        int[] nums = {5, 7, 8, 3, 9, 10, 11, 12, 13, 14, 15, 2};
+        int target = 9;
 
         new RunTime() {
             @Override
             public void codeBlocks() {
                 //此处输入需要计算运行时间的代码
+                solution.twoSum(nums, target);
 
             }
         }.print();
