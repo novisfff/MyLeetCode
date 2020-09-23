0e703c468daa56e532a387779877257be41b381b
npuZhangYuFei <156125813@qq.com>
2020-09-23 20:52:56 +0800

    rewrite runtime class

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import utils.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         //此处输入不计算时间的初始化代码
 
@@ -10,14 +10,12 @@ public class Main {
         int[] nums = {5, 7, 8, 3, 9, 10, 11, 12, 13, 14, 15, 2};
         int target = 9;
 
-        new RunTime() {
-            @Override
-            public void codeBlocks() {
-                //此处输入需要计算运行时间的代码
-                solution.twoSum(nums, target);
+        new RunTime(() -> {
+            //此处输入需要计算运行时间的代码
 
-            }
-        }.print();
+            solution.twoSum(nums, target);
+
+        }).start(100).printMs();
 
     }
 }
--- src/utils/RunTime.java
+++ src/utils/RunTime.java
@@ -2,13 +2,60 @@ package utils;
 
 import java.util.Date;
 
-public abstract class RunTime {
-    public abstract void codeBlocks();
+public class RunTime implements Runnable {
 
-    public void print() {
-        long lastTime = new Date().getTime();
-        codeBlocks();
-        long currentTime = new Date().getTime();
-        System.out.println("Running Time: " + (currentTime - lastTime) + "ms");
+    /* 运行的方法体 */
+    private Runnable target;
+
+    /* 持续时间 */
+    private long lastTime;
+
+
+    public RunTime(Runnable target) {
+        this.target = target;
     }
+
+    /**
+     * create by: zyf
+     * description: 开始执行
+     * create time: 2020/9/23
+     *
+     * @param times 执行次数
+     * @return utils.RunTime
+     */
+    public RunTime start(int times) {
+
+        long totalLastTime = 0;
+        for (int i = 0; i < times + 2; i++) {
+
+            long sTime = System.nanoTime();
+            target.run();
+
+            if (i != 1 && i != times + 1) {
+                totalLastTime += System.nanoTime() - sTime;
+            }
+
+        }
+
+        this.lastTime = totalLastTime / times;
+        return this;
+    }
+
+    @Override
+    public void run() {
+    }
+
+    /**
+     * create by: zyf
+     * description: 打印执行时间（单位毫秒）
+     * create time: 2020/9/23
+     *
+     * @param
+     * @return void
+     */
+    public void printMs() {
+
+        System.out.println("Running Time: " + lastTime / (1000000.0) + "ms");
+    }
+
 }
